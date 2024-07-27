069e682cff10a12b6faf31a4c8ab80137b478592
xena123klaus <155030022+xena123klaus@users.noreply.github.com>
2024-07-27 16:42:02 +0000

    first question

--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,5 +1,22 @@
 #write functions here, don't add input('') statements here!
-print("hello")
+class Stock:
+    def __init__(stock_instance, symbol, company_name):
+        stock_instance.__symbol = symbol
+        stock_instance.__company_name = company_name
+
+    def get_symbol(stock_instance):
+        return stock_instance.__symbol
+    
+    def get_company_name(stock_instance):
+        return stock_instance.__company_name
+    
+def main():
+    microsoft_stock = Stock("MSFT", "Microsoft")
+
+    print(f"Company: {microsoft_stock.get_company_name()}")
+    print(f"Symbol: {microsoft_stock.get_symbol()}")
+
+    
 
 
 
